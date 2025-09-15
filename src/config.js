6298385a309bdead1f3ae577dbf5dfebc7e55379
Andrés Alegría <andresalegria@live.com>
2025-09-15 11:30:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -318,7 +318,14 @@ export default {
             border: '#c3a7c5',
         },
          ],
-        
+  images: [
+        {
+          src: 'Photo2.jpg', 
+          position: 'top',
+          title: 'Rothschild-Giraffes',
+          author: 'Murchison Falls',
+        },
+      ],         
       location: {
         center: [31.506, 2.166],
         zoom: 10.75,
